f786589c796e7ddc797d61181fc167e151623a3b
Eric Lopez <eric.lopez@enti.cat>
2024-02-07 10:14:23 +0100

    SuperBasic Chat

--- Xarxes2.cpp
+++ Xarxes2.cpp
@@ -1,12 +1,134 @@
 
 
 #include <iostream>
+#include <conio.h>
 #include <SFML/Network.hpp>
+
+void RunClient();
+void RunServer();
+
+unsigned short port = 3000;
 
 int main()
 {
-    std::cout << "Hello World!\n";
-    sf::TcpSocket socket;
+	std::cout << "Select" << std::endl << "Client -> C" << std::endl << "Server -> S" << std::endl;
+	
+	char mode = ' ';
+
+	do 
+	{
+		if (_kbhit())
+		{
+			mode = _getch();
+		}
+	} while (mode != 'C' && mode != 'c' && mode != 'S' && mode != 's');
+
+	switch (mode)
+	{
+	case 'C': 
+	case 'c':
+	{
+		RunClient();
+		break;
+	}
+	case 'S':
+	case 's':
+	{
+		RunServer();
+		break;
+	}
+	default:
+		break;
+	}
+
+	while (true)
+	{
+
+	}
 }
 
+void RunClient()
+{
+	std::cout << "Client";
 
+	sf::TcpSocket socket;
+	sf::Socket::Status status = socket.connect("10.40.3.123", port);
+
+	if(status != sf::Socket::Done) 
+	{
+		std::cout << std::endl << "Error on connect to the server";
+		return;
+	}
+
+	while (true)
+	{
+		std::cout << std::endl << "Next Message: ";
+		std::string message;
+		std::getline(std::cin, message);
+
+		char data[100];
+
+		int stringSize = message.length();
+		for(int i = 0; i < stringSize; i++) 
+		{
+			char c = message[i];
+			data[i] = c;
+		}
+
+
+		if(socket.send(data, 100) != sf::Socket::Done) 
+		{
+			std::cout << std::endl << "Error sending message";
+		}
+	}
+}
+
+void RunServer()
+{
+	std::cout << "Server";
+
+	sf::TcpListener listener;
+
+	if(listener.listen(port) != sf::Socket::Done)
+	{
+		std::cout << std::endl << "Error on Start Listener";
+		return;
+	}
+
+	sf::IpAddress ipAddress = sf::IpAddress::getLocalAddress();
+	std::cout << std::endl << "Listening on IP: " + ipAddress.toString();
+
+	sf::TcpSocket client;
+
+	if(listener.accept(client) != sf::Socket::Done)
+	{
+		std::cout << std::endl << "Error on accept Client";
+		return;
+	}
+
+	std::cout << std::endl << "Client Connected: " << client.getRemoteAddress().toString();
+
+	while (true) 
+	{
+		char data[100];
+		std::size_t received;
+
+		std::string message;
+
+		if (client.receive(data, 100, received) != sf::Socket::Done) 
+		{
+			std::cout << std::endl << "Error on receive message";
+		}
+		else
+		{
+			for (size_t i = 0; i < received; i++)
+			{
+				char c = data[i];
+				message += c;
+			}
+			std::cout << std::endl << message;
+
+		}
+	}
+
+}
