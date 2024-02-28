f3e055809936abf10fa74622653016e139c5195a
Eriicloo <eric.lopez@enti.cat>
2024-02-28 10:50:29 +0100

    Windows creation

--- Xarxes2.cpp
+++ Xarxes2.cpp
@@ -3,11 +3,13 @@
 #include <iostream>
 #include <conio.h>
 #include <SFML/Network.hpp>
+#include <SFML/Graphics.hpp>
 
 #include "Chat.h"
 
 void RunClient();
 void RunServer();
+void RunWindows();
 
 unsigned short port = 3000;
 
@@ -16,6 +18,9 @@ int main()
 	std::cout << "Select" << std::endl << "Client -> C" << std::endl << "Server -> S" << std::endl;
 	
 	char mode = ' ';
+
+	RunWindows();
+
 
 	do 
 	{
@@ -42,6 +47,7 @@ int main()
 		break;
 	}
 
+
 	while (true)
 	{
 
@@ -65,4 +71,34 @@ void RunServer()
 	std::cout << "Server";
 
 	Chat* chat = Chat::Server(port);
+}
+
+void RunWindows()
+{
+	sf::RenderWindow windows;
+
+	windows.create(sf::VideoMode(800, 600), "Chat");
+	windows.setFramerateLimit(60);
+
+	sf::Text label;
+	sf::Font font;
+
+	font.loadFromFile("Minecraft.ttf");
+
+
+	label.setFont(font);
+	label.setCharacterSize(16);
+	label.setFillColor(sf::Color::Cyan);
+	label.setString("Mamamelo no mamaba");
+	label.setOrigin(0.5, 0.5);
+
+	label.setPosition(windows.getSize().x * 0.5, windows.getSize().y * 0.5);
+
+
+	while (windows.isOpen())
+	{
+		windows.clear(sf::Color::Black);
+		windows.draw(label);
+		windows.display();
+	}
 }
